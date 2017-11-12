4f176694b277f85db6eebf10d197275cff6c8914
Sagar <sagarach65@gmail.com>
2017-11-12 16:12:58 +0545

    added sign up page

--- app/src/main/java/com/example/sagar/sicklab/LoginPage.java
+++ app/src/main/java/com/example/sagar/sicklab/LoginPage.java
@@ -1,7 +1,11 @@
 package com.example.sagar.sicklab;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        TextView tv1 = (TextView) findViewById(R.id.textView2);
+        tv1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUP.class);
+
+                startActivity(intent);
+
+            }
+        });
+
+
+        TextView tv2 = (TextView) findViewById(R.id.textView5);
+        tv2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUP.class);
+
+                startActivity(intent);
+
+            }
+        });
     }
 }
