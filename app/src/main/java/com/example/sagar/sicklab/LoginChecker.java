4c34c2b036745f4da60ac09d115436f4aef95946
Sagar <sagarach65@gmail.com>
2017-11-12 12:42:37 +0545

    Initial commit

---
+++ app/src/main/java/com/example/sagar/sicklab/LoginChecker.java
@@ -0,0 +1,18 @@
+package com.example.sagar.sicklab;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sagar on 11/12/2017.
+ */
+
+public class LoginChecker extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
